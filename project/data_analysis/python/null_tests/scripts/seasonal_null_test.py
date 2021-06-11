5d9961a7373a8260c979addaab7f7fb1754039d9
laposta-l <laposta-l@ll-a2c1.in2p3.fr>
2021-06-11 14:03:06 +0200

    update null tests

--- project/data_analysis/python/null_tests/scripts/seasonal_null_test.py
+++ project/data_analysis/python/null_tests/scripts/seasonal_null_test.py
@@ -4,8 +4,8 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 import sys, os
-from itertools import combinations_with_replacement as cwr
 from itertools import combinations
+from itertools import product
 
 d = so_dict.so_dict()
 d.read_from_file(sys.argv[1])
@@ -26,157 +26,188 @@ pspy_utils.create_directory(output_data_dir)
 spectra = ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
 modes = ["TT", "TE", "ET", "EE"]
 
+lmax = d["lmax"]
+l_pivot = 1000
+lranges_labels = ["full", "low", "high"]
+lranges = [[0, lmax], [0, l_pivot], [l_pivot, lmax]]
+
 #### SEASON NULL TESTS ####
 if n_surveys < 2:
     sys.exit("Cannot make season null tests with only 1 season.")
 
-spectra_names = [[m for m in cwr(arrays[i], 2)] for i in range(n_surveys)]
-survey_differences = [m for m in combinations(surveys, 2)]
-index_differences = [m for m in combinations(np.arange(n_surveys), 2)]
+# cross spectra name list
+cross_spectra = []
+for i1, surv1 in enumerate(surveys):
+    for j1, arr1 in enumerate(arrays[i1]):
+        for i2, surv2 in enumerate(surveys):
+            if i2 < i1: continue
+            for j2, arr2 in enumerate(arrays[i2]):
+                if j2 < j1 and i1 == i2: continue
+                cross_spectra.append("%s-%sx%s-%s" % (surv1, arr1, surv2, arr2))
 
-chi2_array = {}
+# cross arrays name list
+arrays_full_list = np.concatenate(arrays)
+cross_array_dict = {"%sx%s" % (arr1, arr2): [] for (
+                        arr1, arr2) in product(arrays_full_list, repeat=2)}
+
+cross_ps_dict = {}
+for xspec in cross_spectra:
+    surv_arr1, surv_arr2 = xspec.split("x")
+    surv1, arr1 = surv_arr1.split("-")
+    surv2, arr2 = surv_arr2.split("-")
+    cross_array_dict["%sx%s" % (arr1, arr2)].append(xspec)
+
+    lb, ps = so_spectra.read_ps(
+                  spec_dir + "Dl_%s_%sx%s_%s_cross.dat" % (
+                  surv1, arr1, surv2, arr2), spectra = spectra)
+    cross_ps_dict[surv1, arr1, surv2, arr2] = [lb, ps]
+
+ps_dict = {}
+residual_ps_dict = {}
+chi2_array = []
 chi2_dict = {}
 
-spectra_dict = {}
-residual_dict = {}
+for xarray in cross_array_dict:
+    diff_spectra_list = [m for m in combinations(cross_array_dict[xarray], 2)]
+    # Compute every A-B differences for xarray
+    for diff_spectra in diff_spectra_list:
 
-for index, (surv1, surv2) in enumerate(survey_differences):
+        chi2_list = []
+        ndof_list = []
 
-    chi2_array[surv1, surv2] = []
-    for (spec1, spec2) in spectra_names[index_differences[index][0]]:
+        xspec_A, xspec_B = diff_spectra
 
-        if not (spec1, spec2) in spectra_names[index_differences[index][1]]:
-            continue
+        surv_arr_A1, surv_arr_A2 = xspec_A.split("x")
+        surv_A1, arr_A1 = surv_arr_A1.split("-")
+        surv_A2, arr_A2 = surv_arr_A2.split("-")
 
-        else:
-            line = []
-            ndof_line = []
+        surv_arr_B1, surv_arr_B2 = xspec_B.split("x")
+        surv_B1, arr_B1 = surv_arr_B1.split("-")
+        surv_B2, arr_B2 = surv_arr_B2.split("-")
 
-            lb, ps1 = so_spectra.read_ps(
-                          spec_dir + "Dl_%s_%sx%s_%s_cross.dat" % (
-                          surv1, spec1, surv1, spec2), spectra = spectra)
+        # Read power spectra
 
-            lb, ps2 = so_spectra.read_ps(
-                          spec_dir + "Dl_%s_%sx%s_%s_cross.dat" % (
-                          surv2, spec1, surv2, spec2), spectra = spectra)
+        lb, psA = cross_ps_dict[surv_A1, arr_A1, surv_A2, arr_A2]
 
-            cov11 = np.load(cov_dir + "analytic_cov_%s_%sx%s_%s_%s_%sx%s_%s.npy" % (
-                        surv1, spec1, surv1, spec2, surv1, spec1, surv1, spec2))
+        lb, psB = cross_ps_dict[surv_B1, arr_B1, surv_B2, arr_B2]
 
-            cov22 = np.load(cov_dir + "analytic_cov_%s_%sx%s_%s_%s_%sx%s_%s.npy" % (
-                        surv2, spec1, surv2, spec2, surv2, spec1, surv2, spec2))
+        Nbins = len(lb)
 
-            cov12 = np.load(cov_dir + "analytic_cov_%s_%sx%s_%s_%s_%sx%s_%s.npy" % (
-                        surv1, spec1, surv1, spec2, surv2, spec1, surv2, spec2))
+        # Load covariance matrices
+        covAA = np.load(cov_dir + "analytic_cov_%s_%sx%s_%s_%s_%sx%s_%s.npy" % (
+            surv_A1, arr_A1, surv_A2, arr_A2, surv_A1, arr_A1, surv_A2, arr_A2))
 
-            # Compute residual covariance matrix
-            res_cov = cov11 + cov22 - 2 * cov12
+        covBB = np.load(cov_dir + "analytic_cov_%s_%sx%s_%s_%s_%sx%s_%s.npy" % (
+            surv_B1, arr_B1, surv_B2, arr_B2, surv_B1, arr_B1, surv_B2, arr_B2))
 
-            # TT+TE+EE chi2
-            ps1_ttteee = np.concatenate([ps1[mode] for mode in modes])
-            ps2_ttteee = np.concatenate([ps2[mode] for mode in modes])
-            residual_ps = ps1_ttteee - ps2_ttteee
-            chi2 = residual_ps @ np.linalg.solve(res_cov, residual_ps)
-            chi2_dict["all", surv1, surv2, spec1, spec2] = [chi2, len(residual_ps)]
-            line.append(chi2)
-            ndof_line.append(len(residual_ps))
+        covAB = np.load(cov_dir + "analytic_cov_%s_%sx%s_%s_%s_%sx%s_%s.npy" % (
+            surv_A1, arr_A1, surv_A2, arr_A2, surv_B1, arr_B1, surv_B2, arr_B2))
 
-            # Loop on TT, TE, ET, EE
-            for mode in modes:
+        # Save power spectra and errors
+        stdAA = { mode : np.sqrt(so_cov.selectblock(covAA, modes,
+                            n_bins = Nbins, block = mode + mode).diagonal())
+                  for mode in modes}
 
-                # Select block
-                res_covblock = so_cov.selectblock(res_cov, modes,
-                                                 n_bins = len(lb),
-                                                 block = mode + mode)
+        stdBB = { mode : np.sqrt(so_cov.selectblock(covBB, modes,
+                            n_bins = Nbins, block = mode + mode).diagonal())
+                  for mode in modes}
 
-                # Compute power spectrum and errors
-                res_std = np.sqrt(res_covblock.diagonal())
-                res_ps = ps1[mode] - ps2[mode]
+        ps_dict[surv_A1, arr_A1, surv_A2, arr_A2] = [lb, psA, stdAA]
+        ps_dict[surv_B1, arr_B1, surv_B2, arr_B2] = [lb, psB, stdBB]
 
-                # Compute chi2
-                chi2 = res_ps @ np.linalg.solve(res_covblock, res_ps)
-                chi2_dict[mode, surv1, surv2, spec1, spec2] = [chi2, len(lb)]
-                line.append(chi2)
-                ndof_line.append(len(lb))
+        # Compute residuals and errors
+        res_cov = covAA + covBB - 2 * covAB
+        res_ps_d = { mode : psA[mode] - psB[mode] for mode in modes}
 
-                # Low-ell (ell < 1000) chi2
-                id_low = np.where(lb < 1000)
-                low_ps = res_ps[id_low]
-                low_covblock = res_covblock[np.min(id_low):np.max(id_low)+1,
-                                            np.min(id_low):np.max(id_low)+1]
-                chi2_low = low_ps @ np.linalg.solve(low_covblock, low_ps)
-                chi2_dict[mode, surv1, surv2, spec1, spec2, "low"] = [chi2_low, len(id_low[0])]
-                line.append(chi2_low)
-                ndof_line.append(len(id_low[0]))
+        # Save residuals and errors
+        std_res = { mode : np.sqrt(so_cov.selectblock(res_cov, modes,
+                               n_bins = Nbins, block = mode + mode).diagonal())
+                    for mode in modes}
+        residual_ps_dict[surv_A1, arr_A1, surv_A2, arr_A2,
+                         surv_B1, arr_B1, surv_B2, arr_B2] = [lb, res_ps_d, std_res]
 
-                # High-ell (ell > 1000) chi2
-                id_high = np.where(lb > 1000)
-                high_ps = res_ps[id_high]
-                high_covblock = res_covblock[np.min(id_high):np.max(id_high)+1,
-                                             np.min(id_high):np.max(id_high)+1]
-                chi2_high = high_ps @ np.linalg.solve(high_covblock, high_ps)
-                chi2_dict[mode, surv1, surv2, spec1, spec2, "high"] = [chi2_high, len(id_high[0])]
-                line.append(chi2_high)
-                ndof_line.append(len(id_high[0]))
+        for i, lr in enumerate(lranges):
 
-                # Spectra dict
-                covblock1 = so_cov.selectblock(cov11, modes,
-                                              n_bins = len(lb),
+            id = np.where((lb >= lr[0]) & (lb <= lr[1]))
+            res_ps = np.concatenate([res_ps_d[mode][id] for mode in modes])
+
+            cut_res_cov = []
+            for mode1 in modes:
+                line = []
+                for mode2 in modes:
+                    line.append(
+                        so_cov.selectblock(
+                            res_cov, modes, n_bins = Nbins,
+                            block = mode1 + mode2
+                            )[np.ix_(id[0], id[0])]
+                        )
+                cut_res_cov.append(line)
+            cut_res_cov = np.block(cut_res_cov)
+            print(cut_res_cov.shape)
+
+            # Compute and save chi2
+            chi2 = res_ps @ np.linalg.solve(cut_res_cov, res_ps)
+            chi2_dict["all", surv_A1, arr_A1, surv_A2, arr_A2,
+                      surv_B1, arr_B1, surv_B2, arr_B2] = [chi2, len(res_ps)]
+            chi2_list.append(chi2)
+            ndof_list.append(len(res_ps))
+
+
+
+        for mode in modes:
+            res_covblock = so_cov.selectblock(res_cov, modes,
+                                              n_bins = Nbins,
                                               block = mode + mode)
-                covblock2 = so_cov.selectblock(cov22, modes,
-                                              n_bins = len(lb),
-                                              block = mode + mode)
-                std1 = np.sqrt(covblock1.diagonal())
-                std2 = np.sqrt(covblock2.diagonal())
+            res_ps = res_ps_d[mode]
 
-                spectra_dict[mode, surv1, spec1, spec2] = [lb, ps1[mode], std1]
-                spectra_dict[mode, surv2, spec1, spec2] = [lb, ps2[mode], std2]
+            # Compute chi2
+            for i, lr in enumerate(lranges):
+                id = np.where((lb >= lr[0]) & (lb <= lr[1]))
+                cut_ps = res_ps[id]
+                cut_covblock = res_covblock[np.ix_(id[0], id[0])]
+                chi2 = cut_ps @ np.linalg.solve(cut_covblock, cut_ps)
+                chi2_dict[mode, surv_A1, arr_A1, surv_A2, arr_A2,
+                          surv_B1, arr_B1, surv_B2, arr_B2,
+                          lranges_labels[i]] = [chi2, len(id[0])]
+                chi2_list.append(chi2)
+                ndof_list.append(len(id[0]))
 
-                # Residual dict
-                residual_dict[mode, surv1, surv2, spec1, spec2] = [lb, res_ps, res_std]
-
-            chi2_array[surv1, surv2].append(line)
+        chi2_array.append(chi2_list)
 
 # Save dicts
-pickle.dump(chi2_array, open(os.path.join(output_data_dir, "chi2_arrays.pkl"), "wb"))
-pickle.dump(spectra_dict, open(os.path.join(output_data_dir, "spectra.pkl"), "wb"))
-pickle.dump(residual_dict, open(os.path.join(output_data_dir, "residual.pkl"), "wb"))
+pickle.dump(chi2_dict, open(os.path.join(output_data_dir, "chi2.pkl"), "wb"))
+pickle.dump(ps_dict, open(os.path.join(output_data_dir, "spectra.pkl"), "wb"))
+pickle.dump(residual_ps_dict, open(os.path.join(output_data_dir, "residuals.pkl"), "wb"))
 
 # Histogram plots
-labels = np.array(["TT+TE+EE", "TT", r"TT ($\ell$ $\leq$ 1000)", r"TT ($\ell$ $\geq$ 1000)",
-                       "TE", r"TE ($\ell$ $\leq$ 1000)", r"TE ($\ell$ $\geq$ 1000)",
-                       "ET", r"ET ($\ell$ $\leq$ 1000)", r"ET ($\ell$ $\geq$ 1000)",
-                       "EE", r"EE ($\ell$ $\leq$ 1000)", r"EE ($\ell$ $\geq$ 1000)",
-                       "None", "None"])
+
+lranges = np.array(lranges)
+low = (np.min(lb), lranges[1,1])
+high = (lranges[2,0], lmax)
+
+
+labels = np.array(["TT+TE+EE", "TT", "TE", "ET", "EE",
+                  "TT+TE+EE ($%d\leq\ell\leq%d$)" % low,
+                  "TT ($%d\leq\ell\leq%d$)" % low,
+                  "TE ($%d\leq\ell\leq%d$)" % low,
+                  "ET ($%d\leq\ell\leq%d$)" % low,
+                  "EE ($%d\leq\ell\leq%d$)" % low,
+                  "TT+TE+EE ($%d\leq\ell\leq%d$)" % high,
+                  "TT ($%d\leq\ell\leq%d$)" % high,
+                  "TE ($%d\leq\ell\leq%d$)" % high,
+                  "ET ($%d\leq\ell\leq%d$)" % high,
+                  "EE ($%d\leq\ell\leq%d$)" % high])
+
 labels = labels.reshape(3, 5)
 
-tot_chi2_array = []
+chi2_array = np.array(chi2_array)
+fig, axes = plt.subplots(3, 5, figsize = (20, 12))
 
-for index, (surv1, surv2) in enumerate(survey_differences):
-
-    chi2_array_s1s2 = np.array(chi2_array[surv1, surv2], dtype = np.float)
-    tot_chi2_array.append(chi2_array_s1s2)
-
-    fig, axes = plt.subplots(3, 5, figsize = (15, 9))
-
-    for i in range(len(chi2_array_s1s2[0])):
-        axes[i//5, i%5].hist(chi2_array_s1s2[:, i], label = labels[i//5, i%5])
-        axes[i//5, i%5].axvline(ndof_line[i], ymin = -1, ymax = +2,
-                                color = "k", ls = "--")
-        axes[i//5, i%5].legend(frameon=False)
-    axes[2, -1].axis('off')
-    axes[2, -2].axis('off')
-    plt.tight_layout()
-    plt.savefig(os.path.join(output_plot_dir + "chi2_season_null_%s_%s.pdf" % (surv1, surv2)))
-
-tot_chi2_array = np.vstack(tot_chi2_array)
-fig, axes = plt.subplots(3, 5, figsize = (15, 9))
-for i in range(len(tot_chi2_array[0])):
-    axes[i//5, i%5].hist(tot_chi2_array[:, i], label = labels[i//5, i%5])
-    axes[i//5, i%5].axvline(ndof_line[i], ymin = -1, ymax = +2,
+for i in range(len(chi2_array[0])):
+    axes[i%3, i//3].hist(chi2_array[:, i], label = labels[i%3, i//3])
+    axes[i%3, i//3].axvline(ndof_list[i], ymin = -1, ymax = +2,
                             color = "k", ls = "--")
-    axes[i//5, i%5].legend(frameon = False)
-axes[2, -1].axis('off')
-axes[2, -2].axis('off')
+    axes[i%3, i//3].legend(frameon=False)
+
 plt.tight_layout()
-plt.savefig(os.path.join(output_plot_dir, "chi2_season_null.pdf"))
+plt.savefig(os.path.join(output_plot_dir + "chi2_season_null.pdf"))
